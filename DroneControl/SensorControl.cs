b96294ae32289988a8f4b4e1695f7d54259c96fe
Henrik Kunzelmann <hkunzelmann@web.de>
2016-11-12 00:36:59 +0100

    SensorControl mit Standardwerte initalisieren

--- DroneControl/SensorControl.cs
+++ DroneControl/SensorControl.cs
@@ -25,9 +25,10 @@ namespace DroneControl
         {
             if (drone == null)
                 throw new ArgumentNullException(nameof(drone));
-            this.Drone = drone;
 
+            this.Drone = drone;
             this.Drone.OnDataChange += Drone_OnDataChange;
+            UpdateData(drone.Data);
         }
 
         private void Drone_OnDataChange(object sender, DataChangedEventArgs e)
@@ -38,69 +39,7 @@ namespace DroneControl
                 return;
             }
 
-
-            SuspendLayout();
-
-            if (!float.IsNaN(e.Data.Gyro.Pitch) && !float.IsNaN(e.Data.Gyro.Roll))
-                artificialHorizon.SetAttitudeIndicatorParameters(e.Data.Gyro.Pitch, -e.Data.Gyro.Roll);
-            if (!float.IsNaN(e.Data.Gyro.Yaw))
-                headingIndicator.SetHeadingIndicatorParameters((int)e.Data.Gyro.Yaw);
-
-            calibrateGyroButton.Enabled = e.Data.State != DroneState.Armed && e.Data.State != DroneState.Flying;
-            calibrationRunningText.Visible = e.Data.Gyro.InCalibration;
-
-            orientationLabel.Text = string.Format("Roll: {0} Pitch: {1} Yaw: {2}",
-                Formatting.FormatDecimal(e.Data.Gyro.Roll, 2),
-                Formatting.FormatDecimal(e.Data.Gyro.Pitch, 2),
-                Formatting.FormatDecimal(e.Data.Gyro.Yaw, 2));
-
-            rotationLabel.Text = string.Format("Rotation x: {0} y: {1} z: {2}",
-                Formatting.FormatDecimal(e.Data.Gyro.GyroX, 2),
-                Formatting.FormatDecimal(e.Data.Gyro.GyroY, 2),
-                Formatting.FormatDecimal(e.Data.Gyro.GyroZ, 2));
-
-            float ax = e.Data.Gyro.AccelerationX;
-            float ay = e.Data.Gyro.AccelerationY;
-            float az = e.Data.Gyro.AccelerationZ;
-
-            float len = (float)Math.Sqrt(ax * ax + ay * ay + az * az);
-            accelerationLabel.Text = string.Format("Acceleration x: {0} y: {1} z: {2} len: {3} g",
-                Formatting.FormatDecimal(ax, 2),
-                Formatting.FormatDecimal(ay, 2),
-                Formatting.FormatDecimal(az, 2),
-                Formatting.FormatDecimal(len, 2));
-
-            float mx = e.Data.Gyro.MagnetX;
-            float my = e.Data.Gyro.MagnetY;
-            float mz = e.Data.Gyro.MagnetZ;
-            float magLen = (float)Math.Sqrt(mx * mx + my * my + mz * mz);
-            magnetLabel.Text = string.Format("Magnet x: {0} y: {1} z: {2}{3}Magnet strengh: {4} µT",
-                Formatting.FormatDecimal(mx, 2),
-                Formatting.FormatDecimal(my, 2),
-                Formatting.FormatDecimal(mz, 2),
-                Environment.NewLine,
-                Formatting.FormatDecimal(magLen, 2));
-
-            temperatureLabel.Text = string.Format("Temperature: {0}°C",
-                Formatting.FormatDecimal(e.Data.Gyro.Temperature, 2));
-
-            batteryVoltageLabel.Text = string.Format("Battery voltage: {0} V",
-                Formatting.FormatDecimal(e.Data.BatteryVoltage, 2));
-
-            pressureLabel.Text = string.Format("Pressure: {0} hPa",
-                Formatting.FormatDecimal(e.Data.Baro.Pressure, 2, 4));
-
-            humidityLabel.Text = string.Format("Humidity: {0} %RH",
-                Formatting.FormatDecimal(e.Data.Baro.Humidity, 2, 3));
-
-            temperatureBaroLabel.Text = string.Format("Temperature: {0}°C",
-                Formatting.FormatDecimal(e.Data.Baro.Temperature, 2));
-
-            altitudeLabel.Text = string.Format("Altitude: {0} m",
-                Formatting.FormatDecimal(e.Data.Baro.Altitude, 2, 4));
-
-            ResumeLayout();
-            Invalidate(true);
+            UpdateData(e.Data);
         }
 
         private void calibrateGyroButton_Click(object sender, EventArgs e)
@@ -111,6 +50,72 @@ namespace DroneControl
         private void calibrateMagnetButton_Click(object sender, EventArgs e)
         {
             Drone.SendPacket(new PacketCalibrateGyro(true), true);
+        }
+
+        private void UpdateData(DroneData data)
+        {
+            SuspendLayout();
+
+            if (!float.IsNaN(data.Gyro.Pitch) && !float.IsNaN(data.Gyro.Roll))
+                artificialHorizon.SetAttitudeIndicatorParameters(data.Gyro.Pitch, -data.Gyro.Roll);
+            if (!float.IsNaN(data.Gyro.Yaw))
+                headingIndicator.SetHeadingIndicatorParameters((int)data.Gyro.Yaw);
+
+            calibrateGyroButton.Enabled = data.State != DroneState.Armed && data.State != DroneState.Flying;
+            calibrationRunningText.Visible = data.Gyro.InCalibration;
+
+            orientationLabel.Text = string.Format("Roll: {0} Pitch: {1} Yaw: {2}",
+                Formatting.FormatDecimal(data.Gyro.Roll, 2),
+                Formatting.FormatDecimal(data.Gyro.Pitch, 2),
+                Formatting.FormatDecimal(data.Gyro.Yaw, 2));
+
+            rotationLabel.Text = string.Format("Rotation x: {0} y: {1} z: {2}",
+                Formatting.FormatDecimal(data.Gyro.GyroX, 2),
+                Formatting.FormatDecimal(data.Gyro.GyroY, 2),
+                Formatting.FormatDecimal(data.Gyro.GyroZ, 2));
+
+            float ax = data.Gyro.AccelerationX;
+            float ay = data.Gyro.AccelerationY;
+            float az = data.Gyro.AccelerationZ;
+
+            float len = (float)Math.Sqrt(ax * ax + ay * ay + az * az);
+            accelerationLabel.Text = string.Format("Acceleration x: {0} y: {1} z: {2} len: {3} g",
+                Formatting.FormatDecimal(ax, 2),
+                Formatting.FormatDecimal(ay, 2),
+                Formatting.FormatDecimal(az, 2),
+                Formatting.FormatDecimal(len, 2));
+
+            float mx = data.Gyro.MagnetX;
+            float my = data.Gyro.MagnetY;
+            float mz = data.Gyro.MagnetZ;
+            float magLen = (float)Math.Sqrt(mx * mx + my * my + mz * mz);
+            magnetLabel.Text = string.Format("Magnet x: {0} y: {1} z: {2}{3}Magnet strengh: {4} µT",
+                Formatting.FormatDecimal(mx, 2),
+                Formatting.FormatDecimal(my, 2),
+                Formatting.FormatDecimal(mz, 2),
+                Environment.NewLine,
+                Formatting.FormatDecimal(magLen, 2));
+
+            temperatureLabel.Text = string.Format("Temperature: {0}°C",
+                Formatting.FormatDecimal(data.Gyro.Temperature, 2));
+
+            batteryVoltageLabel.Text = string.Format("Battery voltage: {0} V",
+                Formatting.FormatDecimal(data.BatteryVoltage, 2));
+
+            pressureLabel.Text = string.Format("Pressure: {0} hPa",
+                Formatting.FormatDecimal(data.Baro.Pressure, 2, 4));
+
+            humidityLabel.Text = string.Format("Humidity: {0} %RH",
+                Formatting.FormatDecimal(data.Baro.Humidity, 2, 3));
+
+            temperatureBaroLabel.Text = string.Format("Temperature: {0}°C",
+                Formatting.FormatDecimal(data.Baro.Temperature, 2));
+
+            altitudeLabel.Text = string.Format("Altitude: {0} m",
+                Formatting.FormatDecimal(data.Baro.Altitude, 2, 4));
+
+            ResumeLayout();
+            Invalidate(true);
         }
     }
 }
