511f0316409b302c844f8f7632d1d11b6e6d3ec9
Henrik Kunzelmann <hkunzelmann@web.de>
2016-12-03 15:44:26 +0100

    Baro280: Adresse auf AD0_HIGH ändern

--- DroneFirmware/Baro280.cpp
+++ DroneFirmware/Baro280.cpp
@@ -1,6 +1,6 @@
 #include "Baro280.h"
 
-Baro280::Baro280(Config* config) : Baro(config) {
+Baro280::Baro280(Config* config) : Baro(config), bme(BME280_ADDR_SDO_HIGH) {
 }
 
 const char* Baro280::name() {
